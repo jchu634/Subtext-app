fa716bb8d60069196ae91757601cfb71df8d0d84
JCHU634 <87676426+jchu634@users.noreply.github.com>
2024-12-20 03:03:40 +1300

    Removed next initial code and styled file box

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,17 +1,17 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { Funnel_Display } from "next/font/google";
+import { Plus, Trash } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
+
+const funnelDisplay = Funnel_Display({
+  variable: "--font-funnel",
+  subsets: ["latin"],
+});
+
+const toolbarVars = {
+  height: "h-12",
+  rounded: "rounded-md",
+};
 
 declare global {
   interface Window {
@@ -23,17 +23,20 @@ declare global {
         killWindow: () => void;
         spawnSettingsWindow: () => void;
         killSettingsWindow: () => void;
-        createToastOnMainWindow: (title: string, description: string, duration: number) => void;
+        createToastOnMainWindow: (
+          title: string,
+          description: string,
+          duration: number,
+        ) => void;
         setWindowAlwaysOnTop: (alwaysOnTop: boolean) => void;
       };
     };
   }
-  
 }
 
 async function returnPathDirectories() {
   const handle = await window.pywebview.api.spawnFileDialog();
-  
+
   if (handle.length == 0) {
     // User cancelled, or otherwise failed to open a file.
     return;
@@ -42,115 +45,46 @@ async function returnPathDirectories() {
   console.log(handle);
   return handle;
 }
-
-
-function test() {
-  console.log("test");
-}
-
 export default function Home() {
   return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              pages/index.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div>
+      <main className="flex flex-row bg-slate-50 p-4 dark:bg-slate-950">
+        {/* File Menu */}
+        <div
+          className={`h-[80vh] w-[50vw] bg-[#D9D9D9] ${toolbarVars.rounded}`}
+        >
+          <div
+            className={`flex items-center justify-between bg-[#F4A259] pr-2 text-black ${funnelDisplay.className} text-xl font-bold ${toolbarVars.height} ${toolbarVars.rounded}`}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <p className="pl-4">Files</p>
+            <div>
+              <Button variant="ghost" className="p-2">
+                <Plus
+                  strokeWidth={3}
+                  size={24}
+                  className="hover:text-accent-foreground"
+                />
+                <p className={`${funnelDisplay.className} text-lg font-bold`}>
+                  Add New
+                </p>
+              </Button>
+              <Button variant="ghost" className="p-2">
+                <Trash
+                  strokeWidth={3}
+                  size={24}
+                  className="hover:text-accent-foreground"
+                />
+                <p className={`${funnelDisplay.className} text-lg font-bold`}>
+                  Remove All
+                </p>
+              </Button>
+            </div>
+          </div>
+        </div>
+        <div className="pl-4">
+          <Button onClick={returnPathDirectories}>test</Button>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-
-      <Button onClick={returnPathDirectories}>test</Button>
-      <Button onClick={test}>testButton</Button>
-      <Button onClick={returnPathDirectories}>anothertest</Button>
-        
     </div>
   );
 }
