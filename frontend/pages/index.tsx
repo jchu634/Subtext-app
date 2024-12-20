42560bdccf8bc9f9270218be5775734347b59cac
JCHU634 <87676426+jchu634@users.noreply.github.com>
2024-12-21 01:31:50 +1300

    Finished Frontend File menu

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,7 +1,12 @@
+"use client";
 import { Funnel_Display } from "next/font/google";
 import { Plus, Trash } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+
+import Link from "next/link";
+import { useState, useEffect, useRef } from "react";
 
 const funnelDisplay = Funnel_Display({
   variable: "--font-funnel",
@@ -34,55 +39,118 @@ declare global {
   }
 }
 
-async function returnPathDirectories() {
-  const handle = await window.pywebview.api.spawnFileDialog();
+interface file {
+  fullPath: String;
+  fileName: String;
+}
 
-  if (handle.length == 0) {
-    // User cancelled, or otherwise failed to open a file.
-    return;
+export default function Home() {
+  const [files, setFiles] = useState<file[]>([]);
+
+  async function returnPathDirectories() {
+    const handle = await window.pywebview.api.spawnFileDialog();
+
+    if (handle.length == 0) {
+      // User cancelled, or otherwise failed to open a file.
+      return;
+    }
+
+    const newFiles = handle.map((filePath: string) => {
+      const fileName = filePath.split("\\").pop() || filePath;
+      return { fullPath: filePath, fileName: fileName };
+    });
+    setFiles((prevFiles) => {
+      const allFiles = prevFiles.concat(newFiles);
+      const uniqueFiles = allFiles.filter(
+        (file, index, self) =>
+          index === self.findIndex((f) => f.fullPath === file.fullPath),
+      );
+      return uniqueFiles;
+    });
+    console.log(newFiles);
+    return newFiles;
+  }
+  function clearFiles() {
+    setFiles([]);
+  }
+  function removeFile(fullPath: string) {
+    setFiles((prevFiles) =>
+      prevFiles.filter((file) => file.fullPath !== fullPath),
+    );
   }
 
-  console.log(handle);
-  return handle;
-}
-export default function Home() {
+  function mapFiles(file: any, index: number) {
+    // Index is here to stop the warning about needing a key
+
+    return (
+      <div
+        className="flex h-10 w-full min-w-0 items-center justify-between rounded-md bg-[#5E5E5E] pl-2"
+        key={index}
+      >
+        <p className="w-4/5 truncate">{file.fileName}</p>
+        <div className="pl-2 pr-1">
+          <Button
+            variant="ghost"
+            size="icon"
+            className=""
+            onClick={() => removeFile(file.fullPath)}
+          >
+            <Trash
+              strokeWidth={3}
+              size={24}
+              className="hover:text-accent-foreground"
+            />
+          </Button>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div>
       <main className="flex flex-row bg-slate-50 p-4 dark:bg-slate-950">
         {/* File Menu */}
-        <div
-          className={`h-[80vh] w-[50vw] bg-[#D9D9D9] ${toolbarVars.rounded}`}
-        >
+        <div className="flex">
           <div
-            className={`flex items-center justify-between bg-[#F4A259] pr-2 text-black ${funnelDisplay.className} text-xl font-bold ${toolbarVars.height} ${toolbarVars.rounded}`}
+            className={`h-[80vh] w-[50vw] bg-[#D9D9D9] ${toolbarVars.rounded}`}
           >
-            <p className="pl-4">Files</p>
-            <div>
-              <Button variant="ghost" className="p-2">
-                <Plus
-                  strokeWidth={3}
-                  size={24}
-                  className="hover:text-accent-foreground"
-                />
-                <p className={`${funnelDisplay.className} text-lg font-bold`}>
-                  Add New
-                </p>
-              </Button>
-              <Button variant="ghost" className="p-2">
-                <Trash
-                  strokeWidth={3}
-                  size={24}
-                  className="hover:text-accent-foreground"
-                />
-                <p className={`${funnelDisplay.className} text-lg font-bold`}>
-                  Remove All
-                </p>
-              </Button>
+            <div
+              className={`flex items-center justify-between bg-[#F4A259] pr-2 text-black ${funnelDisplay.className} text-xl font-bold ${toolbarVars.height} ${toolbarVars.rounded}`}
+            >
+              <p className="pl-4">Files</p>
+              <div>
+                <Button
+                  variant="ghost"
+                  className="p-2"
+                  onClick={returnPathDirectories}
+                >
+                  <Plus
+                    strokeWidth={3}
+                    size={24}
+                    className="hover:text-accent-foreground"
+                  />
+                  <p className={`${funnelDisplay.className} text-lg font-bold`}>
+                    Add New
+                  </p>
+                </Button>
+                <Button variant="ghost" className="p-2" onClick={clearFiles}>
+                  <Trash
+                    strokeWidth={3}
+                    size={24}
+                    className="hover:text-accent-foreground"
+                  />
+                  <p className={`${funnelDisplay.className} text-lg font-bold`}>
+                    Remove All
+                  </p>
+                </Button>
+              </div>
+            </div>
+            <div className={`space-y-2 p-3 ${funnelDisplay.className}`}>
+              {files.map((file, index) => {
+                return mapFiles(file, index);
+              })}
             </div>
           </div>
-        </div>
-        <div className="pl-4">
-          <Button onClick={returnPathDirectories}>test</Button>
         </div>
       </main>
     </div>
