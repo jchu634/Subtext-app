073825b76feaaa7b96d7ed0a996933c4c3e2d4fd
JCHU634 <87676426+jchu634@users.noreply.github.com>
2024-12-24 04:35:06 +1300

    Added Settings Dialog trigger to navbar

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -3,6 +3,7 @@ import { useTheme } from "next-themes";
 import { Moon, Sun, Settings, Cog } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
+import { SettingsDialog } from "@/components/settingsDialog";
 
 export default function SideNav() {
   const { setTheme } = useTheme();
@@ -10,14 +11,7 @@ export default function SideNav() {
   return (
     <main className={`pywebview-drag-region`}>
       <div className="flex h-screen w-16 items-end justify-center bg-[#5B8E7D] p-4 md:px-2">
-        <Button
-          variant="ghost"
-          size="icon"
-          className="[&_svg]:size-7"
-          onClick={() => window.pywebview.api.spawnSettingsWindow()}
-        >
-          <Cog strokeWidth={2} />
-        </Button>
+        <SettingsDialog />
       </div>
     </main>
   );
