3ad715c6431b70e49e7434b3f63efa5ec4705f31
JCHU634 <87676426+jchu634@users.noreply.github.com>
2024-12-24 20:38:17 +1300

    Restyled settings dialog

--- frontend/components/settingsDialog.tsx
+++ frontend/components/settingsDialog.tsx
@@ -53,79 +53,84 @@ export function SettingsDialog() {
             <Cog strokeWidth={2} />
           </Button>
         </DialogTrigger>
-        <DialogContent className="h-[80vh] w-[80vw] max-w-screen-2xl bg-slate-100 bg-opacity-95 text-black dark:bg-black dark:bg-opacity-80 dark:text-white">
+        <DialogContent className="flex h-[80vh] w-[80vw] max-w-screen-2xl flex-col bg-slate-100 bg-opacity-95 text-black dark:bg-black dark:bg-opacity-80 dark:text-white">
           <DialogHeader>
-            <DialogTitle>Settings</DialogTitle>
+            <DialogTitle className={`text-3xl ${funnelDisplay.className}`}>
+              Settings
+            </DialogTitle>
             <DialogDescription className="text-gray-800 dark:text-slate-400">
-              Placeholder text
+              {/* Placeholder text */}
             </DialogDescription>
           </DialogHeader>
-
-          <div className="flex flex-row items-center space-x-2">
-            <Label htmlFor="defaultSaveLocation">Save Location:</Label>
-            <p
-              className={`${funnelDisplay.className} rounded-md border border-black p-1`}
+          <div className="space-y-4">
+            <div
+              className={`flex flex-row items-center space-x-2 ${funnelDisplay.className}`}
             >
-              {saveLocation.length == 0 ? "Default" : saveLocation}
-            </p>
+              <Label htmlFor="defaultSaveLocation" className="text-lg">
+                Save Location:
+              </Label>
+              <p className="rounded-md border border-black p-1">
+                {saveLocation.length == 0 ? "Default" : saveLocation}
+              </p>
 
-            <Button id="defaultSaveLocation" onClick={returnPathDirectories}>
-              Browse
-            </Button>
-          </div>
+              <Button id="defaultSaveLocation" onClick={returnPathDirectories}>
+                Browse
+              </Button>
+            </div>
 
-          <div
-            className={`flex flex-row items-center space-x-5 ${funnelDisplay.className}`}
-          >
-            <Label htmlFor="viewModeToggle">
-              Enable Extended Subtitle Formats
-            </Label>
-            <Switch
-              defaultChecked={useExtendedFormats}
-              onClick={() => store.send({ type: "toggleExtentedSubtitles" })}
-              className="outline outline-1 outline-gray-700 dark:bg-slate-200 dark:data-[state=checked]:bg-orange-400"
-            ></Switch>
-          </div>
-          <div
-            className={`flex flex-row items-center space-x-5 ${funnelDisplay.className}`}
-          >
-            <Label htmlFor="viewModeToggle" className="border-black">
-              UI Mode:
-            </Label>
-            <ToggleGroup
-              type="single"
-              variant="outline"
-              id="viewModeToggle"
-              defaultValue={theme}
-              value={theme}
-              onValueChange={(value) => {
-                if (value) {
-                  setTheme(value);
-                }
-              }}
+            <div
+              className={`flex flex-row items-center space-x-5 ${funnelDisplay.className}`}
             >
-              <ToggleGroupItem
-                value="dark"
-                onClick={() => setTheme("dark")}
-                className="border-black hover:bg-gray-400 data-[state=on]:bg-orange-300 dark:data-[state=on]:text-black"
+              <Label htmlFor="viewModeToggle" className="text-lg">
+                Enable Extended Subtitle Formats
+              </Label>
+              <Switch
+                defaultChecked={useExtendedFormats}
+                onClick={() => store.send({ type: "toggleExtentedSubtitles" })}
+                className="outline outline-1 outline-gray-700 dark:bg-slate-200 dark:data-[state=checked]:bg-orange-400"
+              ></Switch>
+            </div>
+            <div
+              className={`flex flex-row items-center space-x-5 ${funnelDisplay.className}`}
+            >
+              <Label htmlFor="viewModeToggle" className="border-black text-lg">
+                UI Mode:
+              </Label>
+              <ToggleGroup
+                type="single"
+                variant="outline"
+                id="viewModeToggle"
+                defaultValue={theme}
+                value={theme}
+                onValueChange={(value) => {
+                  if (value) {
+                    setTheme(value);
+                  }
+                }}
               >
-                Dark
-              </ToggleGroupItem>
-              <ToggleGroupItem
-                value="light"
-                onClick={() => setTheme("light")}
-                className="border-black hover:bg-gray-400 data-[state=on]:bg-orange-300"
-              >
-                Light
-              </ToggleGroupItem>
-              <ToggleGroupItem
-                value="system"
-                onClick={() => setTheme("system")}
-                className="border-black hover:bg-gray-400 data-[state=on]:bg-orange-300 dark:data-[state=on]:text-black"
-              >
-                System
-              </ToggleGroupItem>
-            </ToggleGroup>
+                <ToggleGroupItem
+                  value="dark"
+                  onClick={() => setTheme("dark")}
+                  className="border-black hover:bg-gray-400 data-[state=on]:bg-orange-300 dark:data-[state=on]:text-black"
+                >
+                  Dark
+                </ToggleGroupItem>
+                <ToggleGroupItem
+                  value="light"
+                  onClick={() => setTheme("light")}
+                  className="border-black hover:bg-gray-400 data-[state=on]:bg-orange-300"
+                >
+                  Light
+                </ToggleGroupItem>
+                <ToggleGroupItem
+                  value="system"
+                  onClick={() => setTheme("system")}
+                  className="border-black hover:bg-gray-400 data-[state=on]:bg-orange-300 dark:data-[state=on]:text-black"
+                >
+                  System
+                </ToggleGroupItem>
+              </ToggleGroup>
+            </div>
           </div>
         </DialogContent>
       </Dialog>
