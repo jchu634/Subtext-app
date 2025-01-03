c898fafb8074da8cb58bc4375b2235b584212d4f
JCHU634 <87676426+jchu634@users.noreply.github.com>
2025-01-03 22:43:26 +1300

    Added to inference api

--- backend/inference/inference.py
+++ backend/inference/inference.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Request
 import logging
 import os
-# import ffmpeg
+import ffmpeg
 import tempfile
 
 import importlib.util
@@ -10,9 +10,9 @@ import sys
 
 from config import Settings
 from time import sleep
+import pysubs2
 
 
-interrupt = False
 def load_source(modname, filename):
     loader = importlib.machinery.SourceFileLoader(modname, filename)
     spec = importlib.util.spec_from_file_location(modname, filename, loader=loader)
@@ -42,13 +42,32 @@ def extractAudio(path):
     return audio_paths
 
 
-@transcription_router.get("/models", responses={200: {"description": "Success"}, 404: {"description": "Not Found"}})
-def inference(data, current_model="general_insects"):
-    # Get the prediction from the model
-    model = load_source('predict', f'/{current_model}/model.py')
-    prediction = model.inference(data)
+@transcription_router.get("/supported_formats")
+def getSupportedFormats(model):
+    getFormats = load_source('supportedFormats', f'./inference/{model}/api.py')
+    return getFormats.supportedFormats()
 
-    return prediction
+
+@transcription_router.get("/model_sizes")
+def getModelSizes(model):
+    getSizes = load_source('getModels', f'./inference/{model}/api.py')
+    return getSizes.getModels()
+
+
+# TODO Unfinished
+@transcription_router.get("/subtitle")
+def subtitle(paths, model, modelSize, embed, outputFormats, language="auto"):
+    generator = load_source('generateSubtitle', f'./inference/{model}/api.py')
+    for path in paths:
+        subs = pysubs2.load_from_whisper(generator.generateSubtitle(path, modelSize, language))
+        for format in outputFormats:
+            subs.save(f"audio.{format}")
+
+    # TODO EMBED
+    return {200, "OK"}
+
+## Test ##
+
 
 @transcription_router.get("/testWhile")
 def test():
@@ -60,6 +79,7 @@ def test():
     print("This Stopped Running")
     Settings.testEnable = True
     return 1
+
 
 @transcription_router.get("/test")
 def test_interrupt():
