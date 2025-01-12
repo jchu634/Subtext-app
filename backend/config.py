217051248aab772137483b61e343f1ba834ee1a9
JCHU634 <87676426+jchu634@users.noreply.github.com>
2025-01-13 02:54:17 +1300

    Added appName and appAuthor to config

--- backend/config.py
+++ backend/config.py
@@ -6,6 +6,9 @@ library_path = os.path.dirname(os.path.abspath(__file__))
 
 
 class settingsModel(BaseSettings):
+    appName: str = "Subtext"    # "Gas: Generative AI Subtitling"
+    appAuthor: str = "Joshua Chung"
+
     backendUrl: str = "http://localhost:6789"
     outputPath: str = ""
     testEnable: bool = True
