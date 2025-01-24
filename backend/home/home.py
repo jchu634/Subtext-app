786a0b97d537b42c108f0fc94859e948b4c2a215
JCHU634 <87676426+jchu634@users.noreply.github.com>
2025-01-25 01:17:17 +1300

    Added woff and index.txt to home api whitelist

--- backend/home/home.py
+++ backend/home/home.py
@@ -8,35 +8,50 @@ home_router = APIRouter(tags=["Home"])
 frontend_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "frontend")
 
 ####### favicon #######
+
+
 @home_router.get("/favicon.ico", responses={200: {"description": "Success"}, 404: {"description": "Not Found"}})
 def favicon():
     """
         Serves the favicon
     """
-    if os.path.exists(os.path.join(frontend_path,"favicon.ico")):
-        return FileResponse(os.path.join(frontend_path,"favicon.ico"))
+    if os.path.exists(os.path.join(frontend_path, "favicon.ico")):
+        return FileResponse(os.path.join(frontend_path, "favicon.ico"))
     else:
         return ORJSONResponse(content={"error": "File not found"}, status_code=404)
 
+####### Index.txt #######
+
+
+@home_router.get("/index.txt", responses={200: {"description": "Success"}, 404: {"description": "Not Found"}})
+def index():
+    """
+        Serves the favicon
+    """
+    if os.path.exists(os.path.join(frontend_path, "index.txt")):
+        return FileResponse(os.path.join(frontend_path, "index.txt"))
+    else:
+        return ORJSONResponse(content={"error": "File not found"}, status_code=404)
 
 ####### NextJS Build + Static #######
+
+
 @home_router.get("/")
 @home_router.get("/{path:path}", responses={200: {"description": "Success"}, 404: {"description": "Not Found"}})
-def home(request: Request, path: str=None):
+def home(request: Request, path: str = None):
     """
         Serves the NextJS Frontend
-    """     
+    """
     static_file_path = os.path.join(frontend_path, path)
     print(path)
 
-    # Check if static file is a js, css or svg file (To prevent leaking other files)
-    if static_file_path.endswith(".js") or static_file_path.endswith(".css") or static_file_path.endswith(".svg"):
+    # Check if static file is a js, css, svg, woff2 file (To prevent leaking other files)
+    if static_file_path.endswith(".js") or static_file_path.endswith(".css") or static_file_path.endswith(".svg") or static_file_path.endswith(".woff2"):
         # Checks if Static file exists
         if os.path.isfile(static_file_path):
             logging.info(f"Serving {static_file_path}")
             return FileResponse(static_file_path)
-    
-    
+
     if static_file_path.endswith(".html"):
         # Check if frontend exists
         logging.info(f"Serving {static_file_path}")
@@ -46,7 +61,7 @@ def home(request: Request, path: str=None):
     if os.path.exists(os.path.join(frontend_path, f"{os.path.normpath(path)}.html")):
         logging.info(f'Serving {os.path.join(frontend_path, f"{os.path.normpath(path)}.html")}')
         return HTMLResponse(open(os.path.join(frontend_path, f"{os.path.normpath(path)}.html"), "r").read())
-    
+
     # Path is empty (root path)
     if path == "":
         # Check if frontend index exists
