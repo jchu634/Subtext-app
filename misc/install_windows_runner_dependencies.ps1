f40f29d439af8a8bd6f7c1afe1ad5b7fa5231251
JCHU634 <87676426+jchu634@users.noreply.github.com>
2025-04-23 22:48:12 +1200

    Added dependency script for windows github runners

---
+++ misc/install_windows_runner_dependencies.ps1
@@ -0,0 +1,3 @@
+winget install --id=Python.Python.3.11 -e --silent --accept-source-agreements
+winget install --id=JRSoftware.InnoSetup  -e --silent --accept-source-agreements
+winget install --id=7zip.7zip  -e --silent --accept-source-agreements
