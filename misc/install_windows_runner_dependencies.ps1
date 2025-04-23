e2123ad83a9c99169c062462291ef4a2a6aab5b1
JCHU634 <87676426+jchu634@users.noreply.github.com>
2025-04-24 01:18:45 +1200

    Added syncthing to runner dependency

--- misc/install_windows_runner_dependencies.ps1
+++ misc/install_windows_runner_dependencies.ps1
@@ -1,3 +1,4 @@
 winget install --id=Python.Python.3.11 -e --silent --accept-source-agreements
 winget install --id=JRSoftware.InnoSetup  -e --silent --accept-source-agreements
 winget install --id=7zip.7zip  -e --silent --accept-source-agreements
+winget install --id=Syncthing.Syncthing  -e  --silent --accept-source-agreements
